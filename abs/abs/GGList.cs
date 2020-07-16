1c2ca7f7533f60584baea73a7241f4fc2bb7a1b1
ICEworld27 <56315823+ICEworld27@users.noreply.github.com>
2020-07-16 14:38:37 +0500

    Add files via upload

---
+++ abs/abs/GGList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace abs
+{
+    class GGList
+    {
+        private int[] mas;
+        //private int Len;
+        /*
+            int count = 0;
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+        count = numbers.Count; //5
+numbers.Add(6);
+numbers.Add(7);
+count = numbers.Count; //7
+//преобразуем список в массив
+int[] numbers2 = numbers.ToArray<int>();
+        */
+        public GGList(int len)
+        {
+            mas = new int[len];
+        }
+        public void Add(int numb)
+        {
+            Array.Resize(ref mas, mas.Length + 1);
+            mas[mas.Length - 1] = numb;
+        }
+        public void Remove(int ind)
+        {
+           for(int i = ind; i < mas.Length - 1;i++)
+            {
+                int b = i+1;
+                mas[i] = mas[b];
+            }
+            Array.Resize(ref mas, mas.Length - 1);
+        }
+        public void Insert(int ind, int numb)
+        {
+            Array.Resize(ref mas, mas.Length + 1);
+            for (int i = mas.Length - 1; i > ind; i--)
+            {
+                //int s = mas[i++];
+                int q = i-1;
+                mas[i] = mas[q];
+            }
+            mas[ind] = numb;
+        }
+        //Array.Resize(ref mas, mas.Length - 1);
+        public int Len()
+        {
+           return mas.Length;
+        }
+        public int this[int index]
+        {
+            get { return mas[index]; }
+            set { mas[index] = value; }
+        }
+        public override string ToString()
+        {
+            string v = "[";
+            int c;
+            for (int i =0; i < mas.Length-1; i++)
+            {
+                v += " " + this.mas[i] + ",";
+            }
+            v += " " + this.mas[mas.Length - 1] + "]";
+            return v;
+        }
+    }
+}
---
+++ abs/abs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace abs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GGList a = new GGList(5);
+            Console.WriteLine(a);
+            a.Add(5);
+            
+            Console.WriteLine(a);
+            a.Remove(a.Len()-1);
+            Console.WriteLine(a);
+            
+            a[0] = 5;
+
+            Console.WriteLine(a);
+            a.Remove(0);
+            Console.WriteLine(a);
+            a.Insert(1, 9);
+            Console.WriteLine(a);
+        }
+    }
+}
